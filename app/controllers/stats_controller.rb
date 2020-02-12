93af1f5841f165ddfe074d518e05abd6c2d14a66
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-02-12 22:50:33 +0300

    20200212

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -3,6 +3,6 @@
 class StatsController < ApplicationController
   def index
     order = params[:sort] || 'games'
-    @players = Player.left_joins(:goals).group(:id).order("#{order} DESC").uniq
+    @players = Player.left_joins(:goals).left_joins(:day_players).group(:id).order("#{order} DESC").uniq
   end
 end
