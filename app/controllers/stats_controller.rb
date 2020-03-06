fec20aa89b5ee75e6195385f54c85bc01aa3fc72
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-03-06 12:07:51 +0300

    sorting

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,14 +2,14 @@
 
 class StatsController < ApplicationController
   def index
-    @players = Player.left_joins(:goals).where('days >= 4').group(:id).order("#{order} DESC")
+    @players = Player.left_joins(:goals).where('days >= 4').group(:id).order("#{order || 'games'} DESC")
   end
 
   private
 
   def order
-    return params[:sort] if params[:sort].in? %q(days games rate stat)
+    return params[:sort] || 'games' if params[:sort].in? %w(days games rate stat)
     extra = {'count_goals' => 'COUNT(goals)', 'count_goals_days' => '1.0 * COUNT(goals)/days'}
-    extra[params[:sort]] || 'games'
+    extra[params[:sort]]
   end
 end
