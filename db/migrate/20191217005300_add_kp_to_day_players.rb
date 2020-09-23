2004fed89eaccb3b78bb60d753b1af7e560d5848
dvitvitskiy <dvitvitskiy@npoprogress.com>
2020-09-24 00:24:39 +0300

    bump

--- db/migrate/20191217005300_add_kp_to_day_players.rb
+++ db/migrate/20191217005300_add_kp_to_day_players.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddStatToDayPlayers < ActiveRecord::Migration[5.2]
+class AddKpToDayPlayers < ActiveRecord::Migration[5.2]
   def change
     add_column :day_players, :kp, :integer, default: 0
   end
