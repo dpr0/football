fdff47b531b952f351023e4bd721a706e7d83681
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-01-11 03:38:26 +0300

    refactoring

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,26 +15,9 @@ season1 = Season.create(code: 'season1',  name: '2019')
 season2 = Season.create(code: 'season2',  name: '2020')
 season3 = Season.create(code: 'season3',  name: '2021')
 
-#-----------------------------------------------------------------------------------------------
 [season1, season2, season3].each do |season|
   Player.all.each { |pl| pl.stats.create(sport_id: sport.id, season_id: season.id) }
 
   Dir[File.join(Rails.root, 'db', 'seeds', season.code, '*.rb')].sort.each { |seed| load seed }
   Player.update_stats!(season.id)
-end
-#-----------------------------------------------------------------------------------------------
-
-photos = {}
-Dir.foreach("./app/assets/images/recognize") do |filename|
-  next if ['.', '..'].include? filename
-  z = filename.split(" - ")
-  next if z.size < 2
-  photos[z.first] = (photos[z.first] ? photos[z.first] : []) << z.last
-end
-
-photos.each do |key, values|
-  day_id = Day.find_by(date: key.split(" ").last).id
-  values.each do |value|
-    Recognition.create(file_name: "#{key} - #{value}", day_id: day_id)
-  end
 end
