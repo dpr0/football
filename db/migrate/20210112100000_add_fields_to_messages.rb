2569442016a271cd579cad1a60b2b97e08b641b3
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-01-12 13:14:29 +0300

    20210111

--- db/migrate/20210112100000_add_fields_to_messages.rb
+++ db/migrate/20210112100000_add_fields_to_messages.rb
@@ -3,7 +3,7 @@
 class AddFieldsToMessages < ActiveRecord::Migration[6.0]
   def change
     add_column :messages, :message_id,       :integer
-    add_column :messages, :chat_id,          :integer
+    add_column :messages, :chat_id,          :bigint
     add_column :messages, :date,             :string
     add_column :messages, :reply_message_id, :integer
   end
