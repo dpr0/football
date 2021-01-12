fa0fb936ac85f853ad45b422d929e98d678b8ead
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-01-13 01:15:41 +0300

    bot

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://football.krsz.ru/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  config.action_cable.allowed_request_origins = ['*']
+  config.action_cable.disable_request_forgery_protection = true
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
