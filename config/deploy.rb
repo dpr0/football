96275c6ba78d0b8b3e86da34dcdc7a00f0bb1af5
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-01-13 01:01:11 +0300

    bot

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :rbenv_ruby,      '3.0.0'
 set :application,     'football'
 set :repo_url,        'git@github.com:dpr0/football.git'
 set :deploy_user,     'deploy'
-set :linked_files,    fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/master.key', 'config/credentials.yml.enc', '.env')
+set :linked_files,    fetch(:linked_files, []).push('config/cable.yml', 'config/database.yml', 'config/secrets.yml', 'config/master.key', 'config/credentials.yml.enc', '.env')
 set :linked_dirs,     fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'tmp/session_store', 'vendor/bundle', 'public/system', 'public/uploads')
 set :keep_releases,   5
 set :user,            'deploy'
