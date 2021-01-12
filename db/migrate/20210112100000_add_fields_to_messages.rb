b216fc0e25d1e66c6ee213a26be8e16d14a48955
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-01-12 11:38:35 +0300

    20210111

--- db/migrate/20210112100000_add_fields_to_messages.rb
+++ db/migrate/20210112100000_add_fields_to_messages.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddFieldsToPlayers < ActiveRecord::Migration[6.0]
+class AddFieldsToMessages < ActiveRecord::Migration[6.0]
   def change
     add_column :messages, :message_id, :integer
     add_column :messages, :chat_id, :integer
