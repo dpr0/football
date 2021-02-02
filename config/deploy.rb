6fbd2ad21b1147e1377da95413180efde8d2d378
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-02-02 23:46:57 +0300

    puma4

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,18 +24,18 @@ set :ssh_options, {
     auth_methods: %w(publickey password),
     port: 2222
 }
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-set :pty,             true
-set :deploy_via,      :remote_cache
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
+# set :puma_threads,    [4, 16]
+# set :puma_workers,    0
+# set :pty,             true
+# set :deploy_via,      :remote_cache
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :puma_preload_app, true
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, true
 
 namespace :deploy do
   desc 'deploy about:3001 fractal:3002 webcam:3003'
@@ -94,29 +94,9 @@ namespace :deploy do
   desc 'Initial Deploy'
   task :initial do
     on roles(:app) do
-      before 'deploy:restart', 'puma:start'
       invoke 'deploy'
     end
   end
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
-  end
-
   before :starting, :check_revision
-end
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
 end
