7677d95430c5ceb822b39ae3b922b377b2153e71
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-02-03 09:31:02 +0300

    puma4

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,19 @@ lock '3.15.0'
 
 server 'krsz.ru', port: 2222, roles: %w(app db web), primary: true
 
+# set :puma_threads,    [4, 16]
+# set :puma_workers,    0
+# set :pty,             true
+# set :deploy_via,      :remote_cache
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+# set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_access_log, "#{release_path}/log/puma.error.log"
+# set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :puma_preload_app, true
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, true
+
 set :rbenv_ruby,      '3.0.0'
 set :application,     'football'
 set :repo_url,        'git@github.com:dpr0/football.git'
@@ -24,18 +37,6 @@ set :ssh_options, {
     auth_methods: %w(publickey password),
     port: 2222
 }
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-set :pty,             true
-set :deploy_via,      :remote_cache
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
 
 namespace :deploy do
   desc 'deploy about:3001 fractal:3002 webcam:3003'
