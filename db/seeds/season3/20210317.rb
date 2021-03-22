54b6ed4ada80167c547c63e0790f431a25c7ca7d
dvitvitskiy <dvitvitskiy@npoprogress.com>
2021-03-23 01:02:37 +0300

    20210317

--- db/seeds/season3/20210317.rb
+++ db/seeds/season3/20210317.rb
@@ -1,81 +1,81 @@
-s3d202 = Day.create(sport_id: 1, season_id: 3, date: '17.03.2021'.to_date)
+s3d21 = Day.create(sport_id: 1, season_id: 3, date: '17.03.2021'.to_date)
                                                                    #         |  гол | асист| удар |  фол | сейв |привоз|
-s3d202.day_players.create(season_id: 3, team_id: 2, player_id:  4) # шах     |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 2, player_id: 24) # эл      |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 2, player_id: 13) # араз    |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 2, player_id: 12) # саня    |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 2, player_id: 38) # миша    |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 2, player_id:  6) # тигран  |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 2, player_id:  8) # арг     |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 2, player_id:  4) # шах     |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 2, player_id: 24) # эл      |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 2, player_id: 13) # араз    |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 2, player_id: 12) # саня    |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 2, player_id: 38) # миша    |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 2, player_id:  6) # тигран  |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 2, player_id:  8) # арг     |     |     |     |     |     |     |
 
-s3d202.day_players.create(season_id: 3, team_id: 3, player_id: 21) # дима    |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 3, player_id: 17) # андр    |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 3, player_id: 22) # илья    |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 3, player_id: 42) # серега  |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 3, player_id: 50) # миша    |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 3, player_id: 49) # егор    |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 3, player_id: 21) # дима    |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 3, player_id: 17) # андр    |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 3, player_id: 22) # илья    |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 3, player_id: 42) # серега  |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 3, player_id: 50) # миша    |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 3, player_id: 49) # егор    |     |     |     |     |     |     |
 
-s3d202.day_players.create(season_id: 3, team_id: 4, player_id:  9) # вова    |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 4, player_id: 45) # санин   |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 4, player_id: 53) # аслан   |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 4, player_id: 44) # валера  |     |     |     |     |     |     |
-s3d202.day_players.create(season_id: 3, team_id: 4, player_id: 54) # тарасов |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 4, player_id:  9) # вова    |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 4, player_id: 45) # санин   |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 4, player_id: 53) # аслан   |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 4, player_id: 44) # валера  |     |     |     |     |     |     |
+s3d21.day_players.create(season_id: 3, team_id: 4, player_id: 54) # тарасов |     |     |     |     |     |     |
 
-g2059 = Game.create(day_id: s3d202.id, team_left_id: 4, goals_left: 0, goals_right: 2, team_right_id: 2)
+g2059 = Game.create(day_id: s3d21.id, team_left_id: 4, goals_left: 0, goals_right: 2, team_right_id: 2)
 g2059.goals.create(season_id: 3, team_id: 2, player_id: 6, assist_player_id: nil)
 g2059.goals.create(season_id: 3, team_id: 2, player_id: 4, assist_player_id: nil)
-g2060 = Game.create(day_id: s3d202.id, team_left_id: 3, goals_left: 2, goals_right: 0, team_right_id: 2)
+g2060 = Game.create(day_id: s3d21.id, team_left_id: 3, goals_left: 2, goals_right: 0, team_right_id: 2)
 g2060.goals.create(season_id: 3, team_id: 3, player_id: 49, assist_player_id: nil)
 g2060.goals.create(season_id: 3, team_id: 3, player_id: 49, assist_player_id: nil)
-g2061 = Game.create(day_id: s3d202.id, team_left_id: 3, goals_left: 1, goals_right: 0, team_right_id: 4)
+g2061 = Game.create(day_id: s3d21.id, team_left_id: 3, goals_left: 1, goals_right: 0, team_right_id: 4)
 g2061.goals.create(season_id: 3, team_id: 3, player_id: 22, assist_player_id: nil)
-g2062 = Game.create(day_id: s3d202.id, team_left_id: 3, goals_left: 1, goals_right: 0, team_right_id: 2)
+g2062 = Game.create(day_id: s3d21.id, team_left_id: 3, goals_left: 1, goals_right: 0, team_right_id: 2)
 g2062.goals.create(season_id: 3, team_id: 3, player_id: 42, assist_player_id: 21)
-g2063 = Game.create(day_id: s3d202.id, team_left_id: 3, goals_left: 0, goals_right: 2, team_right_id: 4)
+g2063 = Game.create(day_id: s3d21.id, team_left_id: 3, goals_left: 0, goals_right: 2, team_right_id: 4)
 g2063.goals.create(season_id: 3, team_id: 4, player_id: nil, assist_player_id: nil)
 g2063.goals.create(season_id: 3, team_id: 4, player_id: 45, assist_player_id: nil)
-g2064 = Game.create(day_id: s3d202.id, team_left_id: 2, goals_left: 0, goals_right: 2, team_right_id: 4)
+g2064 = Game.create(day_id: s3d21.id, team_left_id: 2, goals_left: 0, goals_right: 2, team_right_id: 4)
 g2064.goals.create(season_id: 3, team_id: 4, player_id: nil, assist_player_id: nil) # ??????
 g2064.goals.create(season_id: 3, team_id: 4, player_id: nil, assist_player_id: nil) # ??????
-g2065 = Game.create(day_id: s3d202.id, team_left_id: 3, goals_left: 0, goals_right: 2, team_right_id: 4)
+g2065 = Game.create(day_id: s3d21.id, team_left_id: 3, goals_left: 0, goals_right: 2, team_right_id: 4)
 g2065.goals.create(season_id: 3, team_id: 4, player_id: 45, assist_player_id: nil)
 g2065.goals.create(season_id: 3, team_id: 4, player_id: nil, assist_player_id: nil)
-g2066 = Game.create(day_id: s3d202.id, team_left_id: 2, goals_left: 0, goals_right: 2, team_right_id: 4)
+g2066 = Game.create(day_id: s3d21.id, team_left_id: 2, goals_left: 0, goals_right: 2, team_right_id: 4)
 g2066.goals.create(season_id: 3, team_id: 4, player_id: 45, assist_player_id: nil)
 g2066.goals.create(season_id: 3, team_id: 4, player_id: 45, assist_player_id: 53)
-g2067 = Game.create(day_id: s3d202.id, team_left_id: 3, goals_left: 0, goals_right: 2, team_right_id: 4)
+g2067 = Game.create(day_id: s3d21.id, team_left_id: 3, goals_left: 0, goals_right: 2, team_right_id: 4)
 g2067.goals.create(season_id: 3, team_id: 4, player_id: 53, assist_player_id: nil)
 g2067.goals.create(season_id: 3, team_id: 4, player_id: 45, assist_player_id: nil)
-g2068 = Game.create(day_id: s3d202.id, team_left_id: 2, goals_left: 1, goals_right: 1, team_right_id: 4)
+g2068 = Game.create(day_id: s3d21.id, team_left_id: 2, goals_left: 1, goals_right: 1, team_right_id: 4)
 g2068.goals.create(season_id: 3, team_id: 2, player_id: 8, assist_player_id: 13)
 g2068.goals.create(season_id: 3, team_id: 4, player_id: 53, assist_player_id: nil)
-g2069 = Game.create(day_id: s3d202.id, team_left_id: 2, goals_left: 1, goals_right: 1, team_right_id: 3)
+g2069 = Game.create(day_id: s3d21.id, team_left_id: 2, goals_left: 1, goals_right: 1, team_right_id: 3)
 g2069.goals.create(season_id: 3, team_id: 2, player_id: nil, assist_player_id: nil) # ??????
 g2069.goals.create(season_id: 3, team_id: 3, player_id: nil, assist_player_id: nil) # ??????
-g2070 = Game.create(day_id: s3d202.id, team_left_id: 4, goals_left: 1, goals_right: 0, team_right_id: 3)
+g2070 = Game.create(day_id: s3d21.id, team_left_id: 4, goals_left: 1, goals_right: 0, team_right_id: 3)
 g2070.goals.create(season_id: 3, team_id: 4, player_id: 45, assist_player_id: nil)
-g2071 = Game.create(day_id: s3d202.id, team_left_id: 4, goals_left: 1, goals_right: 1, team_right_id: 2)
+g2071 = Game.create(day_id: s3d21.id, team_left_id: 4, goals_left: 1, goals_right: 1, team_right_id: 2)
 g2071.goals.create(season_id: 3, team_id: 4, player_id: 45, assist_player_id: nil)
 g2071.goals.create(season_id: 3, team_id: 2, player_id: 8, assist_player_id: nil)
-g2072 = Game.create(day_id: s3d202.id, team_left_id: 3, goals_left: 0, goals_right: 2, team_right_id: 2)
+g2072 = Game.create(day_id: s3d21.id, team_left_id: 3, goals_left: 0, goals_right: 2, team_right_id: 2)
 g2072.goals.create(season_id: 3, team_id: 2, player_id: 8, assist_player_id: nil)
 g2072.goals.create(season_id: 3, team_id: 2, player_id: 8, assist_player_id: nil)
-g2073 = Game.create(day_id: s3d202.id, team_left_id: 4, goals_left: 2, goals_right: 1, team_right_id: 2)
+g2073 = Game.create(day_id: s3d21.id, team_left_id: 4, goals_left: 2, goals_right: 1, team_right_id: 2)
 g2073.goals.create(season_id: 3, team_id: 4, player_id: 9, assist_player_id: nil)
 g2073.goals.create(season_id: 3, team_id: 4, player_id: 45, assist_player_id: nil)
 g2073.goals.create(season_id: 3, team_id: 2, player_id: 4, assist_player_id: nil)
 
-g2074 = Game.create(day_id: s3d202.id, team_left_id: 4, goals_left: 0, goals_right: 1, team_right_id: 3)
+g2074 = Game.create(day_id: s3d21.id, team_left_id: 4, goals_left: 0, goals_right: 1, team_right_id: 3)
 g2074.goals.create(season_id: 3, team_id: 3, player_id: nil, assist_player_id: nil) # ??????
-g2075 = Game.create(day_id: s3d202.id, team_left_id: 2, goals_left: 0, goals_right: 1, team_right_id: 3)
+g2075 = Game.create(day_id: s3d21.id, team_left_id: 2, goals_left: 0, goals_right: 1, team_right_id: 3)
 g2075.goals.create(season_id: 3, team_id: 3, player_id: 22, assist_player_id: nil)
-g2076 = Game.create(day_id: s3d202.id, team_left_id: 4, goals_left: 1, goals_right: 1, team_right_id: 3)
+g2076 = Game.create(day_id: s3d21.id, team_left_id: 4, goals_left: 1, goals_right: 1, team_right_id: 3)
 g2076.goals.create(season_id: 3, team_id: 4, player_id: 45, assist_player_id: nil)
 g2076.goals.create(season_id: 3, team_id: 3, player_id: 17, assist_player_id: 22)
-g2077 = Game.create(day_id: s3d202.id, team_left_id: 4, goals_left: 2, goals_right: 0, team_right_id: 2)
+g2077 = Game.create(day_id: s3d21.id, team_left_id: 4, goals_left: 2, goals_right: 0, team_right_id: 2)
 g2077.goals.create(season_id: 3, team_id: 4, player_id: nil, assist_player_id: 53)
 g2077.goals.create(season_id: 3, team_id: 4, player_id: nil, assist_player_id: nil)
 
-s3d202.day_rates!
+s3d21.day_rates!
 Player.update_stats!(3)
 Day.last.update!(videos: '6inkctXtV_I,YQoM08RZXss,WJy7V95WaAI,6wDFqgHiHQY,1M7TrgyYWcU,1M0Bfs_4Jf4,3sSsnpW903A,udv0QSdcMoE,R_IPNSOBnuQ')
---
+++ db/seeds/season3/20210322.rb
@@ -0,0 +1,105 @@
+s3d22 = Day.create(sport_id: 1, season_id: 3, date: '22.03.2021'.to_date)
+                                                                  #         |  гол | асист| удар |  фол | сейв |привоз|
+s3d22.day_players.create(season_id: 3, team_id: 1, player_id:  2) # коля    |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 1, player_id:  1) # гарик   |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 1, player_id:  5) # адихан  |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 1, player_id: 23) # врач    |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 1, player_id: 38) # миша    |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 1, player_id:  6) # тигран  |     |     |     |     |     |     |
+
+s3d22.day_players.create(season_id: 3, team_id: 2, player_id:  4) # шах     |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 2, player_id: 24) # эл      |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 2, player_id: 13) # араз    |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 2, player_id: 12) # саня    |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 2, player_id: 10) # виталик |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 2, player_id:  7) # сандро  |     |     |     |     |     |     |
+
+s3d22.day_players.create(season_id: 3, team_id: 3, player_id: 21) # дима    |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 3, player_id: 22) # илья    |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 3, player_id: 42) # серега  |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 3, player_id: 54) # тарасов |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 3, player_id:  9) # вова    |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 3, player_id: 44) # валера  |     |     |     |     |     |     |
+
+s3d22.day_players.create(season_id: 3, team_id: 4, player_id:  8) # арг     |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 4, player_id: 11) # саня б  |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 4, player_id: 14) # сана кр |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 4, player_id: 32) # игорь   |     |     |     |     |     |     |
+s3d22.day_players.create(season_id: 3, team_id: 4, player_id:  3) # ден     |     |     |     |     |     |     |
+
+g2078 = Game.create(day_id: s3d22.id, team_left_id: 2, goals_left: 0, goals_right: 1, team_right_id: 3)
+g2078.goals.create(season_id: 3, team_id: 3, player_id: 42, assist_player_id: 22)
+g2079 = Game.create(day_id: s3d22.id, team_left_id: 1, goals_left: 0, goals_right: 1, team_right_id: 3)
+g2079.goals.create(season_id: 3, team_id: 3, player_id: 54, assist_player_id: 22)
+g2080 = Game.create(day_id: s3d22.id, team_left_id: 4, goals_left: 0, goals_right: 2, team_right_id: 3)
+g2080.goals.create(season_id: 3, team_id: 3, player_id: 21, assist_player_id: nil)
+g2080.goals.create(season_id: 3, team_id: 3, player_id: 54, assist_player_id: 21)
+g2081 = Game.create(day_id: s3d22.id, team_left_id: 2, goals_left: 2, goals_right: 1, team_right_id: 3)
+g2081.goals.create(season_id: 3, team_id: 2, player_id: 13, assist_player_id: nil)
+g2081.goals.create(season_id: 3, team_id: 2, player_id: 13, assist_player_id: 10)
+g2081.goals.create(season_id: 3, team_id: 3, player_id: 42, assist_player_id: 22)
+g2082 = Game.create(day_id: s3d22.id, team_left_id: 2, goals_left: 1, goals_right: 0, team_right_id: 1)
+g2082.goals.create(season_id: 3, team_id: 2, player_id: 13, assist_player_id: 4)
+g2083 = Game.create(day_id: s3d22.id, team_left_id: 2, goals_left: 2, goals_right: 0, team_right_id: 4)
+g2083.goals.create(season_id: 3, team_id: 2, player_id: 24, assist_player_id: 12)
+g2083.goals.create(season_id: 3, team_id: 2, player_id: 10, assist_player_id: 7)
+g2084 = Game.create(day_id: s3d22.id, team_left_id: 2, goals_left: 1, goals_right: 2, team_right_id: 3)
+g2084.goals.create(season_id: 3, team_id: 2, player_id: 4, assist_player_id: nil)
+g2084.goals.create(season_id: 3, team_id: 3, player_id: 21, assist_player_id: nil)
+g2084.goals.create(season_id: 3, team_id: 3, player_id: 21, assist_player_id: 22)
+g2085 = Game.create(day_id: s3d22.id, team_left_id: 1, goals_left: 1, goals_right: 1, team_right_id: 3)
+g2085.goals.create(season_id: 3, team_id: 1, player_id: 2, assist_player_id: 38)
+g2085.goals.create(season_id: 3, team_id: 3, player_id: 21, assist_player_id: nil)
+g2086 = Game.create(day_id: s3d22.id, team_left_id: 2, goals_left: 2, goals_right: 0, team_right_id: 4)
+g2086.goals.create(season_id: 3, team_id: 2, player_id: 4, assist_player_id: 13)
+g2086.goals.create(season_id: 3, team_id: 2, player_id: 13, assist_player_id: 10)
+g2087 = Game.create(day_id: s3d22.id, team_left_id: 2, goals_left: 0, goals_right: 1, team_right_id: 1)
+g2087.goals.create(season_id: 3, team_id: 1, player_id: 6, assist_player_id: 2)
+g2088 = Game.create(day_id: s3d22.id, team_left_id: 3, goals_left: 2, goals_right: 0, team_right_id: 1)
+g2088.goals.create(season_id: 3, team_id: 3, player_id: 44, assist_player_id: nil)
+g2088.goals.create(season_id: 3, team_id: 3, player_id: 21, assist_player_id: nil)
+g2089 = Game.create(day_id: s3d22.id, team_left_id: 3, goals_left: 2, goals_right: 0, team_right_id: 4)
+g2089.goals.create(season_id: 3, team_id: 3, player_id: 54, assist_player_id: nil)
+g2089.goals.create(season_id: 3, team_id: 3, player_id: 54, assist_player_id: nil)
+g2090 = Game.create(day_id: s3d22.id, team_left_id: 3, goals_left: 1, goals_right: 2, team_right_id: 2)
+g2090.goals.create(season_id: 3, team_id: 3, player_id: 42, assist_player_id: 44)
+g2090.goals.create(season_id: 3, team_id: 2, player_id: 4, assist_player_id: 10)
+g2090.goals.create(season_id: 3, team_id: 2, player_id: 4, assist_player_id: 13)
+g2091 = Game.create(day_id: s3d22.id, team_left_id: 1, goals_left: 2, goals_right: 0, team_right_id: 2)
+g2091.goals.create(season_id: 3, team_id: 1, player_id: 6, assist_player_id: 2)
+g2091.goals.create(season_id: 3, team_id: 1, player_id: 6, assist_player_id: 23)
+
+g2092 = Game.create(day_id: s3d22.id, team_left_id: 1, goals_left: 2, goals_right: 0, team_right_id: 4)
+g2092.goals.create(season_id: 3, team_id: 1, player_id: 6, assist_player_id: 2)
+g2092.goals.create(season_id: 3, team_id: 1, player_id: 1, assist_player_id: 6)
+g2093 = Game.create(day_id: s3d22.id, team_left_id: 1, goals_left: 2, goals_right: 1, team_right_id: 3)
+g2093.goals.create(season_id: 3, team_id: 1, player_id: 38, assist_player_id: 6)
+g2093.goals.create(season_id: 3, team_id: 1, player_id: 6, assist_player_id: 38)
+g2093.goals.create(season_id: 3, team_id: 3, player_id: 54, assist_player_id: 44)
+g2094 = Game.create(day_id: s3d22.id, team_left_id: 1, goals_left: 1, goals_right: 1, team_right_id: 2)
+g2094.goals.create(season_id: 3, team_id: 1, player_id: 1, assist_player_id: nil)
+g2094.goals.create(season_id: 3, team_id: 2, player_id: 4, assist_player_id: 7)
+g2095 = Game.create(day_id: s3d22.id, team_left_id: 4, goals_left: 0, goals_right: 1, team_right_id: 3)
+g2095.goals.create(season_id: 3, team_id: 3, player_id: 54, assist_player_id: nil)
+g2096 = Game.create(day_id: s3d22.id, team_left_id: 2, goals_left: 1, goals_right: 1, team_right_id: 3)
+g2096.goals.create(season_id: 3, team_id: 2, player_id: 13, assist_player_id: 24)
+g2096.goals.create(season_id: 3, team_id: 3, player_id: 44, assist_player_id: 54)
+g2097 = Game.create(day_id: s3d22.id, team_left_id: 1, goals_left: 1, goals_right: 2, team_right_id: 4)
+g2097.goals.create(season_id: 3, team_id: 1, player_id: 6, assist_player_id: nil)
+g2097.goals.create(season_id: 3, team_id: 4, player_id: nil, assist_player_id: 32)
+g2097.goals.create(season_id: 3, team_id: 4, player_id: 8, assist_player_id: 22)
+g2098 = Game.create(day_id: s3d22.id, team_left_id: 2, goals_left: 0, goals_right: 1, team_right_id: 4)
+g2098.goals.create(season_id: 3, team_id: 4, player_id: 8, assist_player_id: 22)
+g2099 = Game.create(day_id: s3d22.id, team_left_id: 3, goals_left: 1, goals_right: 0, team_right_id: 4)
+g2099.goals.create(season_id: 3, team_id: 3, player_id: 54, assist_player_id: nil)
+g2100 = Game.create(day_id: s3d22.id, team_left_id: 3, goals_left: 2, goals_right: 0, team_right_id: 1)
+g2100.goals.create(season_id: 3, team_id: 3, player_id: 22, assist_player_id: 44)
+g2100.goals.create(season_id: 3, team_id: 3, player_id: 54, assist_player_id: 21)
+g2101 = Game.create(day_id: s3d22.id, team_left_id: 3, goals_left: 2, goals_right: 1, team_right_id: 2)
+g2101.goals.create(season_id: 3, team_id: 3, player_id: 42, assist_player_id: 54)
+g2101.goals.create(season_id: 3, team_id: 3, player_id: 22, assist_player_id: 42)
+g2101.goals.create(season_id: 3, team_id: 2, player_id: 4, assist_player_id: 13)
+
+s3d22.day_rates!
+Player.update_stats!(3)
+# Day.last.update!(videos: '')
